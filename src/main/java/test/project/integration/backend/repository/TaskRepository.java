b8f179cd6f2059a8f1707a4731839af69e61fd6e
Nikita Savelyev <nsavelyev@it-aces.com>
2024-05-26 00:52:27 +0300

    init: добавление сохранения тасок

--- src/main/java/test/project/integration/backend/repository/TaskRepository.java
+++ src/main/java/test/project/integration/backend/repository/TaskRepository.java
@@ -3,7 +3,5 @@ package test.project.integration.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import test.project.integration.backend.entity.TaskEntity;
 
-import java.util.Collection;
-
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
 }
